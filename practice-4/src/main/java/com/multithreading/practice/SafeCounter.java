c5679664848abd5a14cfa773aeb5744f3b53aaaf
Yury <programmerfromearth@gmail.com>
2026-04-13 21:26:24 +0300

    practice-4

--- practice-4/src/main/java/com/multithreading/practice/SafeCounter.java
+++ practice-4/src/main/java/com/multithreading/practice/SafeCounter.java
@@ -6,6 +6,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Потокобезопасный счетчик без состояния гонки.
  */
 public class SafeCounter {
+    private final AtomicInteger count = new AtomicInteger(0);
 
+    public void increment() {
+        count.incrementAndGet();
+    }
+
+    public int get() {
+        return count.get();
+    }
 }
 
