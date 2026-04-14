639d15a819096a41e384acee031df0e69d989350
Yury <programmerfromearth@gmail.com>
2026-04-14 15:30:28 +0300

    practice-6

--- practice-6/src/main/java/com/multithreading/practice/MaxTask.java
+++ practice-6/src/main/java/com/multithreading/practice/MaxTask.java
@@ -6,6 +6,38 @@ import java.util.concurrent.RecursiveTask;
  * Задача для поиска максимального элемента в массиве с использованием ForkJoinPool.
  */
 public class MaxTask extends RecursiveTask<Integer> {
+    private final int[] array;
+    private final int start;
+    private final int end;
+    private static final int TRASHOLD = 2;
 
+    public MaxTask(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int length = end - start;
+        if (length <= TRASHOLD) {
+            return findMax();
+        } else {
+            int mid = (end + start) / 2;
+            MaxTask leftTask = new MaxTask(array, start, mid);
+            MaxTask rightTask = new MaxTask(array, mid, end);
+
+            leftTask.fork();
+
+            int rightResult = rightTask.compute();
+            int leftRestul = leftTask.join();
+
+            return Math.max(rightResult, leftRestul);
+        }
+    }
+
+    private int findMax() {
+        return Math.max(array[start], array[end - 1]);
+    }
 }
 
--- practice-6/src/main/java/com/multithreading/practice/SumTask.java
+++ practice-6/src/main/java/com/multithreading/practice/SumTask.java
@@ -1,11 +1,48 @@
 package com.multithreading.practice;
 
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 /**
  * Задача для суммирования элементов массива с использованием ForkJoinPool.
  */
 public class SumTask extends RecursiveTask<Long> {
+    private final long[] array;
+    private final int start;
+    private final int end;
+    private static final int TRASHOLD = 100;
 
+    public SumTask(long[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end - start;
+        if (length <= TRASHOLD) {
+            return sum();
+        } else {
+            int mid = (end + start) / 2;
+            SumTask left = new SumTask(array, start, mid);
+            SumTask right = new SumTask(array, mid, end);
+
+            final ForkJoinTask<Long> leftFork = left.fork();
+
+            long rightResult = right.compute();
+            long leftRestul = leftFork.join();
+
+            return rightResult + leftRestul;
+        }
+    }
+
+    private long sum() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
 }
 
